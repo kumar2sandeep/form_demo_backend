430fc6efa287018d27f8ed0007b39152d9f5d23e
kumar2sandeep <sandeep.kumar2@telusinternational.com>
2025-05-23 19:47:15 +0530

    url chaneg

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://feedback-form-ui.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://feedback-form-ui.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
